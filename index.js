bf3eaedf29bb50e7fb93f770d86a3c497bbf0baf
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-02-18 14:00:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
 
-var prefix = ("*")
+var prefix = ("-")
 
+let messageevent = message.content.split(" ");
+let event = messageevent[0];
+let eventargs = messageevent.slice(1); 
+    
+  if (event === prefix + "miroir") {
+    var reponse = ["**Event 1 test**","**Event 2 test**","**Event 3 test**","**Event 4 test**"]
+    var resultat = Math.floor((Math.random() * reponse.length));
+    const eembed =new RichEmbed()
+    .setTitle("**__Event__**")
+    .addField("Réponse:",reponse[resultat])
+    .setColor(0xffa500)
+    message.channel.send(eembed)
+    console.log(`Commade event effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`) 
+  }
 
+});
 bot.login(process.env.TOKEN);
