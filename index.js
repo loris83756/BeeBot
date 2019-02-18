96a6c146300e3f5856187a7e74772a5d82c818c0
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-02-18 16:35:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,34 +20,30 @@ let eventargs = messageevent.slice(1);
   if (event === prefix + "event") {
     var reponse = ["Event sur **Master of the grid**: \n http://masterofthegrid.sparklinlabs.com/play/twitch/ewenasse","Event **Minecraft** \n __Serveur:__ Multi0Game.aternos.me \n __Version:__ 1.8","**Event Roblox**"]
     var resultat = Math.floor((Math.random() * reponse.length));
-     let banRaison = eventargs.join(" ").slice(22);
     if(!message.member.hasPermission("MANAGE_ROLES")) 
     return message.channel.send("**:x:Vous n'avez pas la permissin de faire ceci.**"); 
     const eembed =new RichEmbed()
-    .setTitle("**:tada: __Event__**")
-    .addField("Réponse:",reponse[resultat])
+    .addField(":tada: **__Event__**",reponse[resultat])
     .setColor(Math.floor(Math.random() * 16777214) + 1)
     message.channel.send(eembed)
     console.log(`Commade event effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`) 
   }
-  
       
- if (event === prefix + "autreevent") {      
-let messageaevent = message.content.split(" ");
-let aevent = messageaevent[0];
-let aeventargs = messageaevent.slice(1); 
-if (!aeventargs)
-return message.send("**Vous devez écrire une event a créé")        
- let aeventmessage = aeventargs.join(" ").slice(0);
-   if(!message.member.hasPermission("MANAGE_ROLES")) 
-    return message.channel.send("**:x:Vous n'avez pas la permissin de faire ceci.**");       
-    const aeembed =new RichEmbed()
-    .addField("**:tada: Event**",aeventmessage)
+    let messageaevent = message.content.split(" ");
+    let aevent = messageaevent[0];
+    let aeventargs = messageaevent.slice(1);  
+      
+     if (event === prefix + "autreevent") {
+     let aeventr = aeventargs.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_ROLES")) 
+    return message.channel.send("**:x:Vous n'avez pas la permissin de faire ceci.**"); 
+    const eembed =new RichEmbed()
+    .addField("**:tada: __Event__**",aeventargs)
     .setColor(Math.floor(Math.random() * 16777214) + 1)
-    message.channel.send(aeembed)
-    console.log(`Commade aevent effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`) 
- }
-
+    message.channel.send(eembed)
+    console.log(`Commade aevent effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`)
+     }
+   
  if (message.content === prefix + "ping") {
     const pembed = new RichEmbed()
                 .setTitle(`:ping_pong: Pong: ${bot.ping} ms`)
