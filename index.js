13cc2c508bd140a3f35bf24b1e64b27909f9dc8b
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-02-20 21:27:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -301,6 +301,30 @@ console.log(`Commade sondage effectué par: ${message.author} sur ${message.guil
     console.log(`Commade démineur effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`)  
 }
     
+        let messagedes = message.content.split(" "); let des = messagedes[0]; let desargs = messagedes.slice(1); let channel = message.guild.channels.find(name,'d-e-s')
+
+if (des === prefix + "D.E.S") { 
+        if(!channel) return message.send("Je ne trouve pas le channel d-e-s.") 
+        if (!desargs) return message.send("Vous devez écrire un message a envoyé") 
+        let desmessage = desargs.join(" ").slice(0);
+const desembed = new RichEmbed() 
+.addField(`:bee: Utilisateur: **${message.author.username}**`, desmessage) 
+.setThumbnail(message.author.avatarURL) 
+.setColor(0xffa500) 
+.setTimestamp() 
+.setFooter(`Envoyé de: ${message.guild.name}, ${message.guild.iconURL}`) 
+const ldesembed = new RichEmbed() .addField(:crown: Owner: **${message.author.username}**, desmessage) 
+.setThumbnail(message.author.avatarURL) .setColor(0xff0000) 
+.setTimestamp() 
+.setFooter(`Envoyé de: ${message.guild.name}, ${message.guild.iconURL}`) 
+let iddesl = message.author.id === '295211285405237248';{ 
+        if(!iddesl) 
+                return bot.channels.findAll(name,'d-e-s') .map(channel => channel.send(desembed)) 
+        console.log(`Commade D.E.S effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`)
+}
+bot.channels.findAll(name,'d-e-s') .map(channel => channel.send(ldesembed))
+console.log(`Commade D.E.S effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`) 
+                              }
     
 //OFF---------------------------------------------------------------------------------------------------------------------------------------
     
