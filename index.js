e3abd38f9b7d39ecad6434bb198ddc205a445247
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-02-21 21:25:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -306,7 +306,7 @@ console.log(`Commade sondage effectué par: ${message.author} sur ${message.guil
         let messagedes = message.content.split(" "); 
         let des = messagedes[0]; 
         let desargs = messagedes.slice(1); 
-        let channel = message.guild.channels.find(`name`,'d-e-s')
+        let channel = message.guild.channels.find(channel => channel.name ==="d-e-s")
 
 if (des === prefix + "D.E.S") { 
         if(!channel) return message.send("Je ne trouve pas le channel d-e-s.") 
