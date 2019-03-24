c6435a75a1597df4a328527ef29e438702898822
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-03-24 21:44:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,14 @@ var prefix = ("*")
       
       
 bot.on('ready',function() {
-bot.user.setActivity(`Prefix: * |Status: Maintenance|Fin de maintenace 30 mars`, { type: `STREAMING`, url: 'https://www.twitch.tv/loris83756'})
+bot.user.setActivity(`Prefix: * |Status: Maintenance| Fin de maintenance 30 mars`, { type: `STREAMING`, url: 'https://www.twitch.tv/loris83756'})
     console.log("Connecter");
 })
 
+bot.on('message', message => { 
 if (message.content === prefix + "help") {
     const hembed =new RichEmbed()
-    .setTitle("** :regional_indicator_h: HELP**")
-    .addField("$event","Permet de tirer un event au hasard")
-    .addField("$autreevent","Permet de faire une autre event qui n'est pas dans le $autreevent")
-    .addField("$ping","Permet de savoir la latence du bot en ms")
-    .addField("$infobot","Donne des info sur le bot")
-    .addField("$infouser","Donne des information sur l'utilisateur mentionner")
+    .setTitle("**BEEING EST EN MAINTENANCE. FIN DE MAINTENANCE PREVU LE MARS")
     .setColor(Math.floor(Math.random() * 16777214) + 1)
     message.channel.send(hembed)
     console.log(`Commade help effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`) 
