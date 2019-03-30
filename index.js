3c58d4333ffa719c38b34f7e289b96aee85e32e7
loris83756 <43618944+loris83756@users.noreply.github.com>
2019-03-30 14:51:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,60 @@ const bot = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
 
 var prefix = ("*")
+//BIENVENUE / A QUTTER LE SERVEUR-----------------------------------------------------------------------------------------------
 
+
+// AUTO ROLE---------------------------------------------------------------------------------------------------------------------
+
+bot.on(`guildMemberAdd`,member => {
+    var role = member.guild.roles.find(`name`, `Membres`);
+    member.addRole(role)
+})
+//SET GAME-----------------------------------------------------------------------------------------------------------------------
+bot.on('ready',function() {
+bot.user.setActivity(`Prefix: * | ${bot.users.size} users | ${bot.guilds.size} serveurs |`, { type: `STREAMING`, url: 'https://www.twitch.tv/loris83756'})
+    console.log("Connecter");
+});
+//HELP---------------------------------------------------------------------------------------------------------------------------
 bot.on('message', message => {
+if (message.content === prefix + "help") {
+    const embed = new RichEmbed()
+      .setTitle('**......................................................... \n :bee:__Commandes utiles__::bee:** \n **........................................................**')
+      .addField(`*info-bot`,`:small_orange_diamond:Donne des informations sur le bot.`)
+      .addField(`*info-user + [user]`,`:small_orange_diamond:Donne des informations sur l'utilisateur mentioner. `)
+      .addField(`*info-serveur`,`:small_orange_diamond:Donne des informations sur le serveur ou est effectué cette commande. `)
+      .addField(`*serveurs`,`:small_orange_diamond:Donne la liste de tous les serveurs et du nombre de membres ou se trouve Beeing#4440.`)
+      .addField(`*clear + [nombre]`,`:small_orange_diamond:Permet de supprimer le nombre de message indiquer (max 100). `)
+      .addField(`*ping`,`:small_orange_diamond:Donne le temps de réaction du bot en ms.`)
+      .addField(`**......................................................... \n :hammer:__Commandes modérations__::hammer:**`,` \n **.........................................................** `)
+      .addField(`*ban + [utilisateur] + [raison de la sanction]`,`:small_orange_diamond:Permet de bannir des utilisateurs.`)
+      .addField(`*kick + [utilisateur] + [raison de la sanction]`,`:small_orange_diamond:Permet d´expulser des utilisateurs.`)
+      .addField(`*mute (Bientot disponible)`,`:tools: Permet de rendre muet des utilisateurs.`)
+      .addField(`**......................................................... \n :video_game:__Commandes jeu:__:video_game:**`,` \n **.........................................................**`)
+      .addField(`*dé`,`:small_orange_diamond:Tire un nombre de 1 a 6.`)
+      .addField(`*pile/face`,`:small_orange_diamond:Tire pile ou face.`)
+      .addField(`*miroir + [question] `,`:small_orange_diamond:Répond a la question que vous posez.`)
+      .addField(`*démineur (alpha test 1 niveau)`,`Ne tomber pas sur l'une des bombes!`)
+      .addField(`**......................................................... \n :tv:__Autres fonctionalitées:__:tv:**`,` \n **.........................................................**`)
+      .addField(`Bonjour`,`:small_orange_diamond:Dites Bonjour et le Bot vous répondra.`)
+      .addField(`Bienvenue/a quitté le serveur`,`:small_orange_diamond: Dès qu une personne rejoindra votre serveur le Bot lui souhaitera la Bienvenue et à son départ le Bot dira @...#0000 a quitté le serveur. \n :exclamation:Attention il faut posséder un salon nouveau-départ pour pouvoir avoir accès à cette fonctionnalité.:exclamation:`)
+      .addField(`Auto rôle Membres `,`:small_orange_diamond:Ajoute automatiquement le rôle Membres a tout les nouveaux de votre serveur. \n :exclamation: Attention il faut posséder le rôle Membres.:exclamation:`)
+      .setColor(0xffa500)
+      .setTimestamp()
+      .setFooter("Beeing","https://cdn.discordapp.com/attachments/511129543768145930/531464974548205568/Beeing.jpg")
+    message.author.send(embed);
+    console.log(`Commade help effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`)
+  }
+     
+    if (message.content === prefix + "help") {
+     const embed = new RichEmbed()
+      .setTitle('Les commandes vous ont été envoyés en MP :mailbox_with_mail:')
+      .setColor(0xffa500)
+      .setTimestamp()
+      .setFooter("Beeing","https://cdn.discordapp.com/attachments/519530634901717009/559434819671556107/SPOILER_Beeing.png")
+     message.channel.send(embed)
+    console.log(`Commade help (message MP) effectué par: ${message.author} sur ${message.guild.name} a ${message.createdAt}`)   
+    }
 //INFO BOT-----------------------------------------------------------------------------------------------------------------------
     if (message.content === prefix + "info-bot") {
   const embed = new RichEmbed()
