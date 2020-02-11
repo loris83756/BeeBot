b1fd9aac0f13605965d9508c485c1652f28c6156
loris83756 <43618944+loris83756@users.noreply.github.com>
2020-02-11 19:53:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ if (ban === prefix + "ban") {
     let banUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(banargs[0]));
     if (!banUser) 
     return message.channel.send("**:information_source: Vous devez mentionné l'utilisateur a bannir et la raison de la sanction.**");
-    let banRaison = banargs.join(" ").slice(0);
+    let banRaison = banargs.join(" ").slice(1);
     if(!message.member.hasPermission("BAN_MEMBERS")) 
     return message.channel.send("**:x:Vous n'avez pas la permissin de faire ceci.**");
     if(banUser.hasPermission("BAN_MEMBERS")) 
@@ -207,7 +207,7 @@ if (kick === prefix + "kick") {
     let kickUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(kickargs[0]));
     if (!kickUser) 
     return message.channel.send("**:information_source: Vous devez mentionné l'utilisateur a expulser et la raison de la sanction.**");
-    let kickRaison = kickargs.join(" ").slice(0);
+    let kickRaison = kickargs.join(" ").slice(1);
     if(!message.member.hasPermission("KICK_MEMBERS")) 
     return message.channel.send("**:x:Vous n'avez pas la permissin de faire ceci.**");
     if(kickUser.hasPermission("KICK_MEMBERS")) 
@@ -227,7 +227,7 @@ console.log(`Commade kick effectué par: ${message.author} sur ${message.guild.n
 //CLEAR----------------------------------------------------------------------------------------------------------------------------
 let messageclear = message.content.split(" ");
 let clear = messageclear[0];
-let clearargs = messageclear.slice(0); 
+let clearargs = messageclear.slice(1); 
   
 if (clear === prefix + "clear") {
     if (!clearargs[0]) 
