8ddb7a1079a1b1495f7e6e044c5baa8be9144ab2
loris83756 <43618944+loris83756@users.noreply.github.com>
2020-02-12 11:27:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on("guildMemberRemove" , member => {
 // AUTO ROLE---------------------------------------------------------------------------------------------------------------------
 
 bot.on(`guildMemberAdd`,member => {
-    var role = member.guild.roles.find(`name`, `corona`);
+    var role = member.guild.roles.find(`name`, `🥒menbre🥒`);
     member.addRole(role)
 })
 //SET GAME-----------------------------------------------------------------------------------------------------------------------
